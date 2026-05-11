97ace5c1a113328c22ee82b819a6a93f6f02d839
DamianRyh <gawrych.damian@gmail.com>
2026-05-11 10:59:50 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.WFC_CONFIG = {
-  SUPABASE_URL: "https://kpbzjfanleyaiwhzvsor.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_YsLanoIurd3BXKkT4zRN5w_wYoS4H4l",
-  LOCAL_STORAGE_KEY: "warsaw_fc_base_1_0",
-  DEFAULT_EVENT_CODE: "[ S03E12 ]",
-  ADMIN_ROLE: "admin"
-};
